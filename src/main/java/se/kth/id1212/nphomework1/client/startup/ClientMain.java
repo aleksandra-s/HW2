301125f8bb462d47e5d887a1cdd60f9e589daaa1
aleks_uuia3ly <aleks_uuia3ly@130.229.177.115>
2017-11-16 15:30:12 +0100

    fix

--- src/main/java/se/kth/id1212/nphomework1/client/startup/ClientMain.java
+++ src/main/java/se/kth/id1212/nphomework1/client/startup/ClientMain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package se.kth.id1212.nphomework1.client;
+package se.kth.id1212.nphomework1.client.startup;
 
 import se.kth.id1212.nphomework1.client.view.InputHandler;
 
--- src/main/java/se/kth/id1212/nphomework1/server/controller/ServerController.java
+++ src/main/java/se/kth/id1212/nphomework1/server/controller/ServerController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package se.kth.id1212.nphomework1.server;
+package se.kth.id1212.nphomework1.server.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
